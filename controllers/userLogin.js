d5c71847ce09c5406e4f19c2f2bf086b35cc4ddf
CaioHPT <caio.h.teixeira@gmail.com>
2021-11-16 09:05:01 -0300

    update

--- controllers/userLogin.js
+++ controllers/userLogin.js
@@ -1,7 +1,6 @@
 const usuario = require('../models/usuarios')
 const sequelize = require('../models/db')
 
-
 module.exports = (req, res) => {
     usuario.findAll({where: {email: req.body.email}})
     .then((usuario) => {
@@ -14,8 +13,15 @@ module.exports = (req, res) => {
         .then((user)=> {
             console.log(user[0])
             if((user[0][0].senha == req.body.password)){
+                sequelize.query(`select id from usuarios where email='${req.body.email}'`)
+                .then((id) => {
+                    const userId = id[0][0].id
+                    console.log(userId)
+                    localStorage.setItem('id', userId)
+                }).catch((err) => console.log(err))
                 res.redirect('/personalInfo')
             }else{
+                res.redirect('/login')
                 const erro = 'Email ou senha invalidos'
                 console.log(erro)
             }
